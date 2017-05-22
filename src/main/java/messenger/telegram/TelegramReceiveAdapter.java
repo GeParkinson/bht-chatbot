9b80a8792135d985deb78ab020b7b6e8aae51b7b
Christopher Kümmel <kuemmel.christopher@gmail.com>
2017-05-22 20:55:29 +0200

    parse TelegramMessage-Object to message.Message

--- src/main/java/messenger/telegram/TelegramReceiveAdapter.java
+++ src/main/java/messenger/telegram/TelegramReceiveAdapter.java
@@ -3,10 +3,14 @@ package messenger.telegram;
 import com.pengrad.telegrambot.BotUtils;
 import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
+import message.Attachment;
+import message.AttachmentType;
+import message.FileType;
 import message.Messenger;
 
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import java.util.ArrayList;
 
 /**
  * Created by Chris on 5/14/2017.
@@ -28,11 +32,19 @@ public class TelegramReceiveAdapter {
     private message.Message toMessage(com.pengrad.telegrambot.model.Message message) {
         message.Message msg = new message.Message();
 
-        //TODO: Check IDs - String or LONG?
-        msg.setText(message.text());
         msg.setMessenger(Messenger.TELEGRAM);
         msg.setMessageID(message.messageId().longValue());
         msg.setSenderID(message.chat().id());
+
+        ArrayList<Attachment> attachments = new ArrayList<>();
+
+        if(message.audio() != null) attachments.add(new Attachment(Long.valueOf(message.audio().fileId()), AttachmentType.AUDIO, FileType.FILE_ID));
+        if(message.video() != null) attachments.add(new Attachment(Long.valueOf(message.video().fileId()), AttachmentType.VIDEO, FileType.FILE_ID));
+        if(message.voice() != null) attachments.add(new Attachment(Long.valueOf(message.voice().fileId()), AttachmentType.VOICE, FileType.FILE_ID));
+        if(message.document() != null) attachments.add(new Attachment(Long.valueOf(message.document().fileId()), AttachmentType.DOCUMENT, FileType.FILE_ID));
+        if(message.text() != null) msg.setText(message.text());
+
+        msg.setAttachements((Attachment[]) attachments.toArray());
 
         return msg;
     }
