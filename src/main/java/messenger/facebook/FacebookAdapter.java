1bc151372fdf795fce1ca33c365df1a4a9c2f30a
Oliver David <oliverdavid@hotmail.de>
2017-05-28 20:12:33 +0200

    added media support

--- src/main/java/messenger/facebook/FacebookAdapter.java
+++ src/main/java/messenger/facebook/FacebookAdapter.java
@@ -1,5 +1,8 @@
 package messenger.facebook;
 
+import message.Attachment;
+import message.AttachmentType;
+import message.Messenger;
 import messenger.utils.MessengerUtils;
 import org.json.JSONObject;
 
@@ -13,6 +16,8 @@ import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Map;
+
+import static messenger.facebook.FacebookSendAdapter.activateWebhook;
 
 /**
  * Created by Oliver on 5/14/2017.
@@ -41,33 +46,89 @@ public class FacebookAdapter {
     @Consumes("application/json")
     public String ReceiveMessage(String InputMessage) throws IOException {
 
-        System.out.println(InputMessage);
-        System.out.println(accessToken);
+        System.out.println("Input:"+InputMessage);
 
-        String sender ;
-        String message;
+        String sender ="";
+        String message="";
+        Attachment attach = null;
         Boolean isEcho;
         JSONObject obj;
-        try {
-            obj = new JSONObject(InputMessage);
-            sender = obj.getJSONArray("entry").getJSONObject(0).getJSONArray("messaging").getJSONObject(0).getJSONObject("sender").getString("id");
-            message = obj.getJSONArray("entry").getJSONObject(0).getJSONArray("messaging").getJSONObject(0).getJSONObject("message").getString("text");
-            }
-        catch(Exception ex){
-            message="";
-            sender="";
-        }
-        try{
-            obj = new JSONObject(InputMessage);
-            isEcho=obj.getJSONArray("entry").getJSONObject(0).getJSONArray("messaging").getJSONObject(0).getJSONObject("message").getBoolean("is_echo");
-        }
-        catch(Exception ex){
-            isEcho=false;
-        }
 
-        if(message.length()>0&&isEcho==false) {
-            sendTextMessage(sender,message,accessToken);
-        }
+        //message object
+        message.Message msg = new message.Message();
+
+        obj = new JSONObject(InputMessage);
+
+        //check if message
+        if(obj.getJSONArray("entry").getJSONObject(0).getJSONArray("messaging").getJSONObject(0).getJSONObject("message")!=null) {
+
+
+                //sender
+                try {
+
+                    sender = obj.getJSONArray("entry").getJSONObject(0).getJSONArray("messaging").getJSONObject(0).getJSONObject("sender").getString("id");
+                } catch (Exception ex) {}
+
+                //message text
+            try {
+
+                message = obj.getJSONArray("entry").getJSONObject(0).getJSONArray("messaging").getJSONObject(0).getJSONObject("message").getString("text");
+            } catch (Exception ex) {}
+
+            //attachements
+            try {
+                String attType = obj.getJSONArray("entry").getJSONObject(0).getJSONArray("messaging").getJSONObject(0).getJSONObject("message").getJSONArray("attachments").getJSONObject(0).getString("type");
+                String url = obj.getJSONArray("entry").getJSONObject(0).getJSONArray("messaging").getJSONObject(0).getJSONObject("message").getJSONArray("attachments").getJSONObject(0).getJSONObject("payload").getString("url");
+
+                attach=new Attachment();
+                attach.setFileUrl(url);
+
+                switch (attType) {
+                    case "image":
+                        attach.setAttachmentType(AttachmentType.PHOTO);
+                        break;
+                    case "audio":
+                        attach.setAttachmentType(AttachmentType.AUDIO);
+                        break;
+                    case "fallback":
+                        break;
+                    case "file":
+                        attach.setAttachmentType(AttachmentType.DOCUMENT);
+                        break;
+                    case "location":
+                        break;
+                    case "video":
+                        attach.setAttachmentType(AttachmentType.VIDEO);
+                        break;
+                }
+
+                message="attached "+attType;
+
+                Attachment[] atts= new Attachment[1];
+                atts[0]=attach;
+                msg.setAttachements(atts);
+
+                } catch (Exception ex) {}
+
+            //is echo? --> echo message for send messages
+                try {
+                    obj = new JSONObject(InputMessage);
+                    isEcho = obj.getJSONArray("entry").getJSONObject(0).getJSONArray("messaging").getJSONObject(0).getJSONObject("message").getBoolean("is_echo");
+                } catch (Exception ex) {
+                    isEcho = false;
+                }
+
+                if ((message.length() > 0) && isEcho == false) {
+
+                    msg.setText(message);
+                    msg.setMessenger(Messenger.FACEBOOK);
+                    msg.setMessageID(Long.valueOf("85753757347"));
+                    msg.setSenderID(Long.valueOf(sender));
+
+                    MessengerUtils.sendMessage(msg);
+                }
+            }
+
         return "\nReceived\n";
 
     }
@@ -89,19 +150,7 @@ public class FacebookAdapter {
                     webhookToken.equals(request.getParameter("hub.verify_token"))){
                 System.out.println("VERIFIED");
 
-                //Start function to activate webhook
-                Runnable activation = new Runnable() {
-                    public void run() {
-                        try {
-                            Thread.sleep(5000);
-                        } catch (InterruptedException e) {
-                            //e.printStackTrace();
-                        }
-                        sendPostRequest("https://graph.facebook.com/v2.9/me/subscribed_apps?access_token="+accessToken,"");
-                    }
-                };
-
-                new Thread(activation).start();//Call it when you need to run the function
+                activateWebhook();
 
                 return request.getParameter("hub.challenge");
             }
@@ -114,48 +163,8 @@ public class FacebookAdapter {
 
 
 
-    public static void sendTextMessage(String recipient, String message, String accessToken){
-        String payload = "{\"recipient\": {\"id\": \"" + recipient + "\"}, \"message\": { \"text\": \""+message+"\"}}";
-        String requestUrl = "https://graph.facebook.com/v2.6/me/messages?access_token=" + accessToken;
-        try {
-            sendPostRequest(requestUrl, payload);
-        }
-        catch(Exception ex){
-            //System.out.println(ex.getMessage()+" ERROR");
-        }
-        //System.out.println(payload+"------"+requestUrl);
-    }
 
 
-    public static String sendPostRequest(String requestUrl, String payload) {
-        StringBuffer jsonString = new StringBuffer();
-        try {
-            URL url = new URL(requestUrl);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
-            connection.setDoInput(true);
-            connection.setDoOutput(true);
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Accept", "application/json");
-            connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-            OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream(), "UTF-8");
-            writer.write(payload);
-            writer.close();
-            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-
-            String line;
-            while ((line = br.readLine()) != null) {
-                jsonString.append(line);
-            }
-            br.close();
-
-            //System.out.println(connection.getResponseMessage());
-
-            connection.disconnect();
-        } catch (Exception e) {
-            throw new RuntimeException(e.getMessage());
-        }
-        return jsonString.toString();
-    }
 
 }
