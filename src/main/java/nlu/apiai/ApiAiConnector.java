7d92a26e87468d49d561ff361439cc2e78914a5d
glossmann <georg.glossmann@adesso.de>
2017-06-08 18:41:36 +0200

    move api.ai sample code into provided class

--- src/main/java/nlu/apiai/ApiAiConnector.java
+++ src/main/java/nlu/apiai/ApiAiConnector.java
@@ -1,10 +1,10 @@
 package nlu.apiai;
 
-import com.google.gson.Gson;
-import nlu.rasa.model.RasaResponse;
-import org.jboss.resteasy.client.jaxrs.ResteasyClient;
-import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
-import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import message.BotMessage;
+import messenger.utils.MessengerUtils;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,8 +14,7 @@ import javax.ejb.MessageDriven;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriBuilder;
+import java.util.Properties;
 
 /**
  * @author: georg.glossmann@adesso.de
@@ -37,7 +36,7 @@ import javax.ws.rs.core.UriBuilder;
 public class ApiAiConnector implements MessageListener {
 
     private final Logger logger = LoggerFactory.getLogger(ApiAiConnector.class);
-    
+
 
     @PostConstruct
     public void init() {
@@ -47,6 +46,32 @@ public class ApiAiConnector implements MessageListener {
 
     @Override
     public void onMessage(final Message message) {
-        // TODO: implementation
+        // TODO: fix sample code
+        try {
+            BotMessage botMessage = message.getBody(BotMessage.class);
+
+            String sessionID = "";
+            String[] contexts = new String[]{};
+
+            Properties properties = MessengerUtils.getProperties();
+            String token = properties.getProperty("API_AI_TOKEN");
+
+            String language = "en";
+
+            String reqURL = String.format("https://api.api.ai/api/query?query=%s&lang=%s&sessionId=%s", botMessage.getText(), language, sessionID);
+
+            for (String context : contexts) {
+                reqURL = reqURL + "&contexts=" + context;
+            }
+
+            //TODO: Replace Unirest with RESTeasy
+            JSONObject resultJson = Unirest.get(reqURL).header("Authorization", "Bearer " + token).asJson().getBody().getObject();
+
+            resultJson.getJSONObject("result");
+        } catch (JMSException e) {
+            logger.error("Could not process message.", e);
+        } catch (UnirestException e) {
+            logger.error("Request failed!", e);
+        }
     }
 }
