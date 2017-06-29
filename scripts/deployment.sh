f5f92473d8d689d2a5cf6429b0df127f9ee64e4b
glossmann <georg.glossmann@adesso.de>
2017-06-29 21:10:16 +0200

    fix not working; needs to be done manual

--- scripts/deployment.sh
+++ scripts/deployment.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 ./gradlew war
 cd src/main/docker
-# FIX: https://stackoverflow.com/questions/35760760/writable-folder-permissions-in-docker
-chown -R 1000:beuthbot  wildfly/volumes/deployments
 docker-compose down && docker-compose up --build -d
