3ece277e6fdfe037c86432f4e8289f382fa8d068
SoerenJantzen <soerenjantzen@gmx.net>
2017-07-06 16:12:45 +0200

    minor change in traffic lights enum

--- src/main/java/de/bht/chatbot/canteen/model/TrafficLight.java
+++ src/main/java/de/bht/chatbot/canteen/model/TrafficLight.java
@@ -5,7 +5,7 @@ package de.bht.chatbot.canteen.model;
  */
 public enum TrafficLight {
 
-    GREEN("green"), YELLOW("red"), RED("yellow");
+    GREEN("green"), YELLOW("yellow"), RED("red");
 
     private String text;
 
