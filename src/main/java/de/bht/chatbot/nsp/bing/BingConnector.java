de35fb86595ab140dfb95b9751ac7d940bf6221f
glossmann <georg.glossmann@adesso.de>
2017-07-06 18:15:00 +0200

    log bing result

--- src/main/java/de/bht/chatbot/nsp/bing/BingConnector.java
+++ src/main/java/de/bht/chatbot/nsp/bing/BingConnector.java
@@ -215,7 +215,7 @@ public class BingConnector implements MessageListener {
                             case "simple":
                                 BingSimpleResponse bingSimpleResponse = new Gson().fromJson(result.toString(), BingSimpleResponse.class);
                                 bingMessage = new BingMessage(bingSimpleResponse, botMessage);
-                                logger.debug("Received message from bing: {}", bingMessage.getText());
+                                logger.debug("Received message from bing: {}", result);
                                 break;
                             case "detailed":
                                 BingDetailedResponse bingDetailedResponse = new Gson().fromJson(result.toString(), BingDetailedResponse.class);
