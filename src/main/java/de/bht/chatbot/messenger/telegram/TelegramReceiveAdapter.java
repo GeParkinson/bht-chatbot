e50e9223cb4fc4d13cce59b92b361a248326000d
glossmann <georg.glossmann@adesso.de>
2017-07-13 12:41:46 +0200

    try to fix Telegram update

--- src/main/java/de/bht/chatbot/messenger/telegram/TelegramReceiveAdapter.java
+++ src/main/java/de/bht/chatbot/messenger/telegram/TelegramReceiveAdapter.java
@@ -62,12 +62,13 @@ public class TelegramReceiveAdapter {
      */
     @POST
     @Path("/getUpdates")
-    public void getUpdates(final String msg) {
+    public boolean getUpdates(final String msg) {
         Update update = BotUtils.parseUpdate(msg);
         logger.debug("Received new Telegram message: {}", update);
         TelegramMessage message = new TelegramMessage(update.message());
         message.setAttachments(getAttachments(update.message()));
         messageQueue.addInMessage(message);
+        return true;
     }
 
     /**
