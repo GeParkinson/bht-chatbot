97c8c10e07eba12740e21ad5f22fffde85f9c4f8
glossmann <georg.glossmann@adesso.de>
2017-07-17 15:31:11 +0200

    load application configuration from file at runtime

--- services/Global/src/main/java/de/bht/beuthbot/conf/ApplicationBean.java
+++ services/Global/src/main/java/de/bht/beuthbot/conf/ApplicationBean.java
@@ -1,7 +1,12 @@
 package de.bht.beuthbot.conf;
 
 
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.ejb.Stateless;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -23,27 +28,32 @@ public class ApplicationBean implements Application {
      */
     private static final String CONFIGURATION_FILE = "beuthbot.properties";
 
+    /**
+     * slf4j Logger
+     */
+    private final Logger logger = LoggerFactory.getLogger(ApplicationBean.class);
+
+    /**
+     * Properties defined in conf file
+     */
     private Properties properties;
 
     public ApplicationBean() {
-        getProperties();
+        loadProperties();
     }
 
-    private Properties getProperties(){
+    /**
+     * Loading defined properties from conf file
+     * @return
+     */
+    private Properties loadProperties() {
         if (properties == null) {
             this.properties = new Properties();
-            InputStream inputStream = null;
-            try {
-                inputStream = ApplicationBean.class.getResourceAsStream(CONFIGURATION_PATH + CONFIGURATION_FILE);
+            File confFile = new File(CONFIGURATION_PATH + CONFIGURATION_FILE);
+            try (InputStream inputStream = FileUtils.openInputStream(confFile)) {
                 properties.load(inputStream);
             } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                try {
-                    inputStream.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                logger.error("Error while reading file {}", confFile.getAbsolutePath(), e);
             }
         }
         return properties;
@@ -51,10 +61,11 @@ public class ApplicationBean implements Application {
 
     /**
      * Gets the value of configured property
+     *
      * @param property enum of defined property in configuration file
      * @return String of configured value
      */
     public String getConfiguration(final Configuration property) {
-        return getProperties().getProperty(property.name());
+        return loadProperties().getProperty(property.name());
     }
 }
