6f0222f9b8c7dede2ebf837940ce2120c52d0c2c
Oliver David <oliverdavid@hotmail.de>
2017-07-20 17:53:59 +0200

    fixed webhook path

--- services/FaceBook/src/main/java/de/bht/beuthbot/messenger/facebook/FacebookReceiveAdapter.java
+++ services/FaceBook/src/main/java/de/bht/beuthbot/messenger/facebook/FacebookReceiveAdapter.java
@@ -173,7 +173,7 @@ public class FacebookReceiveAdapter {
 
         String fields = "messages, messaging_postbacks, messaging_optins, message_deliveries, message_reads, messaging_payments, messaging_pre_checkouts, messaging_checkout_updates, messaging_account_linking, messaging_referrals, message_echoes";
 
-        String callback_url = application.getConfiguration(Configuration.WEB_URL) + "/getUpdates";
+        String callback_url = application.getConfiguration(Configuration.WEB_URL) + "/facebook/getUpdates";
         Response response = facebookProxy.sendHook("page", callback_url, fields, webhookToken, access_token);
 
         String responseAsString = response.readEntity(String.class);
